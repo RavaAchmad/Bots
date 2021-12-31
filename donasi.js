83d42c4437948abfdc3cb1196270818744da7534
RavaAchmad <75514649+RavaAchmad@users.noreply.github.com>
2021-12-31 09:15:32 +0700

    Add files via upload

---
+++ donasi.js
@@ -0,0 +1,21 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+-
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0812 1203 5575
+2. OVO
+	ㅁ 0812 1203 5575
+3. DANA
+	ㅁ 0812 1203 5575
+	
+`
+	}
+
+exports.donasibot = donasibot
+ 
